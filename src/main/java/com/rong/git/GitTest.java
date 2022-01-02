e77c46b0f202fbac45e5654a1a4505b9b8b20f83
rong <rong@qq.com>
2022-01-02 17:51:53 +0800

    master commit2

--- src/main/java/com/rong/git/GitTest.java
+++ src/main/java/com/rong/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("rong");
         System.out.println("hotfix");
+        System.out.println("master");
     }
 }
